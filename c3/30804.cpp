382b5065952d069aa0d962d46d559248581e8210
Geunyeong Kim <98966788+Dubutoto@users.noreply.github.com>
2025-04-23 16:42:48 +0900

    Cpp Day 9-1

--- c3/30804.cpp
+++ c3/30804.cpp
@@ -7,13 +7,5 @@ int main(){
     ios::sync_with_stdio(false);
     cin.tie(NULL);
 
-    int N;
-    cin >> N;
-
-    vector<int> v(N);
-    for(int i = 0; i < N; i++){
-        cin >> v[i];
-    }
-    
     return 0;
 }
